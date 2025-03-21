71a1d81ece771bef2654c95d10383bebc17fe1a9
swc9803 <swc9803@gmail.com>
2025-03-21 11:46:57 +0900

    타입스크립트 적용

--- src/store/useFishStore.ts
+++ src/store/useFishStore.ts
@@ -1,12 +1,24 @@
-// store/useFishStore.ts
 import { create } from "zustand";
 
-export const useFishStore = create((set) => ({
-	fishColor: "#ffffff",
+interface FishStoreState {
+	fishColor: string;
+	fishSpeed: number;
+	fishScale: number;
+	darkMode: boolean;
+}
+
+interface FishStoreActions {
+	setFishColor: (color: string) => void;
+	setFishSpeed: (speed: number) => void;
+	setFishScale: (scale: number) => void;
+	toggleDarkMode: () => void;
+}
+
+export const useFishStore = create<FishStoreState & FishStoreActions>((set) => ({
+	fishColor: "#e7b518",
 	fishSpeed: 50,
 	fishScale: 1,
 	darkMode: false,
-
 	setFishColor: (color: string) => set({ fishColor: color }),
 	setFishSpeed: (speed: number) => set({ fishSpeed: speed }),
 	setFishScale: (scale: number) => set({ fishScale: scale }),
