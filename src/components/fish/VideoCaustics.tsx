5323a381c6a9b635ba85e5906dd43f14e3b95bef
swc9803 <swc9803@gmail.com>
2025-05-21 10:56:25 +0900

    video 메모리 생성 및 해제 로직 수정

--- src/components/fish/VideoCaustics.tsx
+++ src/components/fish/VideoCaustics.tsx
@@ -1,28 +1,57 @@
-import { useVideoTexture } from "@react-three/drei";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+import { VideoTexture, LinearFilter, RGBFormat, Mesh } from "three";
 
 export const VideoCaustics = () => {
-	const videoTexture = useVideoTexture("/videos/caustics.mp4");
+	const [videoTexture, setVideoTexture] = useState<VideoTexture | null>(null);
+	const meshRef = useRef<Mesh>(null);
 
 	// 메모리 해제
 	useEffect(() => {
-		const videoElement = videoTexture.image;
+		const video = document.createElement("video");
+		video.src = "/videos/caustics.mp4";
+		video.crossOrigin = "anonymous";
+		video.loop = true;
+		video.muted = true;
+		video.autoplay = true;
+		video.playsInline = true;
 
-		return () => {
-			videoTexture.dispose();
-
-			if (videoElement instanceof HTMLVideoElement) {
-				videoElement.pause();
-				videoElement.removeAttribute("src");
-				videoElement.load();
+		const tryPlay = () => {
+			const playPromise = video.play();
+			if (playPromise !== undefined) {
+				playPromise.catch((error) => {
+					console.warn(error);
+				});
 			}
 		};
-	}, [videoTexture]);
+
+		const handleCanPlay = () => {
+			const texture = new VideoTexture(video);
+			texture.minFilter = LinearFilter;
+			texture.magFilter = LinearFilter;
+			texture.format = RGBFormat;
+			setVideoTexture(texture);
+			video.removeEventListener("canplay", handleCanPlay);
+		};
+
+		video.addEventListener("canplay", handleCanPlay);
+		requestAnimationFrame(tryPlay);
+
+		return () => {
+			video.pause();
+			video.removeAttribute("src");
+			video.load();
+			if (videoTexture) {
+				videoTexture.dispose();
+			}
+		};
+	}, []);
 
 	return (
-		<mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.01, 0]} renderOrder={10}>
-			<planeGeometry args={[350, 70]} />
-			<meshBasicMaterial map={videoTexture} transparent opacity={0.3} depthWrite={false} depthTest={false} />
-		</mesh>
+		videoTexture && (
+			<mesh ref={meshRef} rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.01, 0]} renderOrder={10}>
+				<planeGeometry args={[350, 70]} />
+				<meshBasicMaterial map={videoTexture} transparent opacity={0.3} depthWrite={false} depthTest={false} />
+			</mesh>
+		)
 	);
 };
